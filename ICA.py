2c33cdec09fa095de39d81cfac26ca202c497cf8
maliha-afk <24amaliha@gmail.com>
2024-12-01 22:01:34 +0600

    Add files via upload

--- ICA.py
+++ ICA.py
@@ -24,9 +24,14 @@ intereste=Entry(screen,font=("Arial",15,"bold"))
 intereste.place(x=220,y=110)
 
 def calcint():
-    interest=(int(amount.get()))*(int(time.get()))*(int(intereste.get()))/100
-    totalamount=interest+(int(amount.get()))
-    myamount.config(text=f"interest value: {interest} INR \n total amount: {totalamount} INR")
+    try:
+     interest=(int(amount.get()))*(int(time.get()))*(int(intereste.get()))/100
+     totalamount=interest+(int(amount.get()))
+     myamount.config(text=f"interest value: {interest} INR \n total amount: {totalamount} INR")
+
+    except:
+       myamount.config(text=f"please enter valid input",fg="red")
+       
 
 simpleintbut=Button(screen,text="calculate simple interest ",font=("Arial",15,"bold"),fg="black",bg="lightblue",relief="sunken",command=calcint)
 simpleintbut.place(x=180,y=150)
